12b9c1e7775dd2d182c8622ff9e56f68e12877ff
timtimmytimothy <hatleytimothy@gmail.com>
2026-03-27 04:21:45 -0400

    New exercise added

---
+++ Chapter 8 Exercises/message.py
@@ -0,0 +1,11 @@
+# 8-1: Message
+
+# This program defines a function then calls that function
+
+# Function definition
+def message():
+    """This function will print a message describing what I'm learning about"""
+    print('In this chapter, I will be learning about Functions in Python!')
+
+# Calls the function - must call it
+message()
